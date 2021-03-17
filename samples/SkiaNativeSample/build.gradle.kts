00ad148e43c2d4f8a48f6c5e3d0e07220ecddbde
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2021-03-17 23:57:23 +0300

    [Native] Removed garbage

--- samples/SkiaNativeSample/build.gradle.kts
+++ samples/SkiaNativeSample/build.gradle.kts
@@ -71,38 +71,3 @@ kotlin {
         }
     }
 }
-
-/*
-val additionalArguments = mutableMapOf<String, String>()
-
-val casualRun = tasks.named<JavaExec>("run") {
-    systemProperty("skiko.fps.enabled", "true")
-    System.getProperties().entries
-        .associate {
-            (it.key as String) to (it.value as String)
-        }
-        .filterKeys { it.startsWith("skiko.") }
-        .forEach { systemProperty(it.key, it.value) }
-    additionalArguments.forEach { systemProperty(it.key, it.value) }
-}
-
-tasks.register("runSoftware") {
-    additionalArguments += mapOf("skiko.renderApi" to "SOFTWARE")
-    dependsOn(casualRun)
-}
-
-tasks.withType<Test> {
-    systemProperty("skiko.test.screenshots.dir", File(project.projectDir, "src/test/screenshots").absolutePath)
-
-    // Tests should be deterministic, so disable scaling.
-    // On MacOs we need the actual scale, otherwise we will have aliased screenshots because of scaling.
-    if (System.getProperty("os.name") != "Mac OS X") {
-        systemProperty("sun.java2d.dpiaware", "false")
-        systemProperty("sun.java2d.uiScale", "1")
-    }
-}
-
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
-}
-*/
